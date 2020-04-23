98d4fd54c83bedb110f48a557ac2bc82693bbbe9
Carlos Vazquez <cvazquez1976@gmail.com>
2020-04-22 23:12:38 -0400

    * Read from blog api, list latest blogs and link to post

---
+++ pages/blog/index.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch'
+import Link from 'next/link'
+
+function HomePage({homeData}) {
+	return (
+			<div>
+					{homeData.latestBlogs.map(latestBlog => (
+							<div key={latestBlog.id}>
+								<p>	<Link href="/blog/[slug]" as={`/blog/${latestBlog.titleURL}`}>
+										<a>{latestBlog.title}</a>
+									</Link>
+									<br />
+									{latestBlog.contentTeaser}<br />
+								</p>
+							</div>
+						))
+					}
+			</div>
+	)
+}
+
+export async function getStaticProps() {
+	const	res = await fetch("http://dev.react-api.carlosvazquez.org/blog/api", {
+						method:	'GET',
+						cache:	'force-cache'
+					}
+			),
+			homeData = await res.json();
+
+//console.log(homeData)
+
+	return {
+		props:	{
+			homeData,
+		}
+	}
+}
+
+export default HomePage;
