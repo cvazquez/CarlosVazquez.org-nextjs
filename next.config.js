eb9a440ef36eaea65ed2642c162bfcd983a3e777
Carlos Vazquez <cvazquez1976@gmail.com>
2020-05-03 16:24:00 -0400

    * It was returning false and I needed empty string

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	env:	{
 		global	: {
 			title	: 'Adventures of Carlos',
-			apiURL	: "http://" + (process.env.NODE_ENV === "development" && "dev.") + "api.carlosvazquez.org"
+			apiURL	: "http://" + (process.env.NODE_ENV === "development" ? "dev." : "") + "api.carlosvazquez.org"
 		}
 	}
 }
