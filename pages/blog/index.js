2eeee0710cacdf748e9718ff7412ec857e8cc969
Carlos Vazquez <cvazquez1976@gmail.com>
2020-07-08 15:17:04 -0400

    Update blog intro

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -20,10 +20,58 @@ export default function Index({homeData}) {
 							</header>
 
 							<article>
-								<p>My name is Carlos and I am a FullStack Website Developer. I developed this website using React and Node.js, on the Next.JS framework.
-									And I created a simple Node.JS Express API communicating with MySQL, where these blog posts are stored.</p>
-								<p>I plan to showcase projects I developed during my career as a FullStack Website Developer, in addition to personal projects, including the code for this blog.</p>
-								<p>Outside of coding, I enjoy living an active and healthy lifestyle, through travelling, running, bikng, and other outdoor and physical activites. I will try to post about the various adventures I've been on, from international vacations to a 50 mile Ultra Marathon.</p>
+								<p>My name is Carlos Vazquez, and I am a FullStack Website Developer and adventurer.</p>
+
+								<p>I developed this website using the following technologies:</p>
+									<ul>
+										<li>Javascript / Node.js</li>
+										<li>React</li>
+										<li>Next.js</li>
+										<li>MySQL</li>
+										<li>Express</li>
+										<li>Bootstrap</li>
+										<li>CSS (SASS)</li>
+									</ul>
+
+								<p>Links to the code are posted below.</p>
+
+								<h2>Projects</h2>
+
+									<h3>Blog</h3>
+									<p>This Blog is built on the Next.js React framework.
+										<br />
+										[<a href="https://github.com/cvazquez/CarlosVazquez.org-nextjs" target="GitHub-NextJS">code</a>]
+									</p>
+
+									<h3>API</h3>
+									<p>Using Node.js, Express and MySQL, I created a simple API, that Next.js calls to deliver this blogs content, saves comments, and administers the site.
+										<br />
+										[<a href="https://github.com/cvazquez/CarlosVazquez.org-ExpressNodeAPI" target="GitHub-API">code</a>].
+									</p>
+
+									<h3>Images</h3>
+									<p>Posts with an <a href="/blog/images">image slideshow</a>, are hosted on Flickr.</p>
+
+									<p>I created a Node.js script, using oAuth, that communicates with the Flickr API and stores photo data into a MySQL DB.
+										<br />
+										[<a href="https://github.com/cvazquez/flickr-import" target="GitHub-FlickrImport">code</a>].
+									</p>
+
+									<h3>Admin</h3>
+									<p>Using "create react app", I built a CMS type admin site, to create and edit these posts, add new categories and series, and link Flickr Albums to Posts.
+										The interface uses my API, to read and write data.
+										<br />
+										[<a href="https://github.com/cvazquez/CarlosVazquez.org-AdminReact" target="GitHub-AdminReact">code</a>].
+									</p>
+
+
+								<h2>Hobbies</h2>
+								<p>Outside of coding, I enjoy living an active and healthy lifestyle.</p>
+
+								<p>I like travelling, running, bikng, and other outdoor and physical activites.</p>
+
+								<p>I hope to post about the adventures I've been on, from international vacations, to a 50 mile Ultra Marathon.</p>
+
 								<p>Checkout my various blog post categories and latest blogs for more information.</p>
 							</article>
 						</main>
